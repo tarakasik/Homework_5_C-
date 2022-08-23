cc7cd996d0f8c1705e23565700e9a8ba36f545f4
Stanislav Volokhov <lamantina123@gmail.com>
2022-08-23 16:56:50 +0400

    Task 34

---
+++ Task#36/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] GetRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+Console.Clear();
+int[] array = GetRandomArray(10,1,100);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int sum=0;
+for (int i = 1; i < array.Length; i+=2)
+{
+    sum+=array[i];
+}
+Console.WriteLine($"The sum of the elements standing in odd positions: {sum}");
---
+++ Task#38/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+Console.Clear();
+int[] array = Random(10,1,100);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+int min = array[0];
+int max = array[0];
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i]>max)
+    {
+        max=array[i];
+    }
+    if (array[i]<min)
+    {
+        min=array[i];
+    }
+}
+Console.WriteLine($"The difference between the maximum and minimum array elements: {max-min}");
